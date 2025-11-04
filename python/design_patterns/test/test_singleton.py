fae6d272d083ad5e1e9c17aeea1e34c96557dad3
fchen <fm_c@hotmail.com>
2025-11-04 09:48:31 +0000

    add test

--- python/design_patterns/test/test_singleton.py
+++ python/design_patterns/test/test_singleton.py
@@ -1,5 +1,19 @@
-# from design_patterns.singleton.singleton import PersonSingleton
+from design_patterns.singleton.singleton import PersonSingleton
 import pytest
 
-def test_basic():
-    assert True == False
+def test_initialize():
+    person = PersonSingleton(name="John Doe", age=30)
+    assert person.get_instance().name == "John Doe"
+    assert person.get_instance().age == 30
+
+def test_second_instance_exception():
+    with pytest.raises(Exception) as excinfo:
+        another_person = PersonSingleton(name="Jane Doe", age=25)
+
+    assert "Singleton can't be instantiated more than ones!" in str(excinfo.value)
+
+def test_print_data(capfd):
+    person = PersonSingleton.get_instance()
+    person.print_data()
+    captured = capfd.readouterr()
+    assert captured.out.strip() == f"Name: {person.name}, age: {person.age}"
