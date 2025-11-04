e97abf68373ffb1c6cd26ad1e83283a38dbca98b
fchen <fm_c@hotmail.com>
2025-11-04 10:42:03 +0000

    formatting

--- python/design_patterns/test/test_builder.py
+++ python/design_patterns/test/test_builder.py
@@ -1,6 +1,17 @@
 from design_patterns.builder.builder import HouseBuilder
 import pytest
 
+
+def test_default_house_builder():
+    house_builder = HouseBuilder()
+    house = house_builder.build()
+    assert house.windows == 0
+    assert house.doors == 0
+    assert house.rooms == 0
+    assert house.has_garage is False
+    assert house.has_garden is False
+
+
 def test_house_builder():
     house_builder = HouseBuilder()
     house_builder.windows = 4
@@ -14,13 +25,4 @@ def test_house_builder():
     assert house.doors == 2
     assert house.rooms == 3
     assert house.has_garage is True
-    assert house.has_garden is False
-
-def test_default_house_builder():
-    house_builder = HouseBuilder()
-    house = house_builder.build()
-    assert house.windows == 0
-    assert house.doors == 0
-    assert house.rooms == 0
-    assert house.has_garage is False
     assert house.has_garden is False
